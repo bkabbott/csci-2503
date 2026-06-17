cbc365747eb5a4b361c7657f8abe417521719556
Brian Abbott <hi@brianabbott.com>
2026-06-17 15:41:58 -0400

    compiled c program

---
+++ week5/input_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // output
+    printf("Enter a number: ");
+
+    // input
+    char input[100];
+    scanf("%s", input);
+
+    // output
+    printf("You enetered %s\n", input);
+    return 0;
+}
