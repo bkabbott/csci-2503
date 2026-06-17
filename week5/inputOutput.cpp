0c25ad3a9d1471969b807f6f5af76e55e837f057
Brian Abbott <hi@brianabbott.com>
2026-06-17 16:00:30 -0400

    wrote c and cpp code

---
+++ week5/inputOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int num;
+
+    // output
+    std::cout << "Enter a number: ";
+
+    // input
+    std::cin >> num;
+
+    // output
+    std::cout << "You entered: " << num << std::endl;
+    return 0;
+}
---
+++ week5/inputOutput2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    // output
+    cout << "Enter a number: ";
+
+    // input
+    cin >> num;
+
+    // output
+    cout << "You entered: " << num << std::endl;
+    return 0;
+}
